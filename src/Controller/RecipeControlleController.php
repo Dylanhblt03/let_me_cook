622c6fbbf27755b8209cea77c1e9607aff266cb1
Dylanhblt03 <dylan.hblt03@gmail.com>
2026-04-01 14:49:02 +0200

    #

--- src/Controller/RecipeControlleController.php
+++ src/Controller/RecipeControlleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
 use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,17 +11,40 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class RecipeControlleController extends AbstractController
 {
-    #[Route('/recettes', name: 'app_recettes')]
+    #[Route('/recipes', name: 'app_recettes')]
     public function index(Request $request, RecipeRepository $recipeRepository): Response
     {
         $difficulte = $request->query->get('difficulte');
         $duree = $request->query->get('duree');
         $auteur = $request->query->get('auteur');
 
-        $recipes = $recipeRepository->findByFilters($difficulte, $duree, $auteur);
+        if ($difficulte || $duree || $auteur) {
+            $recipes = $recipeRepository->findByFilters($difficulte, $duree, $auteur);
+        } else {
+            $recipes = $recipeRepository->findAll();
+        }
 
         return $this->render('all_recipe/index.html.twig', [
             'recipes' => $recipes,
+        ]);
+    }
+
+    #[Route('/recipes/{id}', name: 'app_recette_detail')]
+    public function show(Recipe $recipe): Response
+    {
+        return $this->render('all_recipe/detail.html.twig', [
+            'recipe' => $recipe,
+        ]);
+    }
+
+    #[Route('/recipes/author/{author}', name: 'app_recettes_auteur', priority: 1)]
+    public function byAuthor(string $author, RecipeRepository $recipeRepository): Response
+    {
+        $recipes = $recipeRepository->findBy(['author' => $author]);
+
+        return $this->render('all_recipe/author.html.twig', [
+            'recipes' => $recipes,
+            'author' => $author,
         ]);
     }
 }
